114c8dc64317847de08f75312ce6c1c33f91967b
Osman.ordu <orduosmann@gmail.com>
2025-12-27 08:35:29 +0300

    main | General Added

---
+++ src/db/columns/index.ts
@@ -0,0 +1,2 @@
+export { currencyColumns, quickTransactionColumns } from './currencyColumns';
+
---
+++ src/db/props/index.ts
@@ -0,0 +1,5 @@
+// Database props types and interfaces will be defined here
+// This file is a placeholder for future database-related props
+
+export {};
+
